dadd2553bad20104b0dc93b47788ba88e332f3c7
codnjs779 <codnjs779@naver.com>
2022-01-27 00:12:24 +0900

    result page

---
+++ src/components/result/Result.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styles from "./Result.module.css";
+import img from "../../images/sample.PNG";
+import BlackButton from "../blackBtn/BlackButton";
+import YellowButton from "../yellowBtn/YellowButton";
+const Result = () => {
+    return (
+        <div className={styles.resultBox}>
+            <section className={styles.scoreSection}>
+                <div className={styles.title}>나와 상대의 궁합은</div>
+                <div className={styles.score}>00점</div>
+
+                <div className={styles.heartIcon}>
+                    <i className="fas fa-heart"></i>
+                </div>
+                <div className={styles.container}>
+                    <div className={styles.progress}>
+                        <div className={styles.progressBar}>
+                            <div className={styles.circle}></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.scoreBox}>
+                    <div>
+                        <img src={img} alt="" />
+                        <div>별자리</div>
+                        <div>00점</div>
+                    </div>
+
+                    <div>
+                        <img src={img} alt="" />
+                        <div>12지신</div>
+                        <div>00점</div>
+                    </div>
+
+                    <div>
+                        <img src={img} alt="" />
+                        <div>혈액형</div>
+                        <div>00점</div>
+                    </div>
+
+                    <div>
+                        <img src={img} alt="" />
+                        <div>MBTI</div>
+                        <div>00점</div>
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.result}>
+                <div className={styles.title}>결과 해석</div>
+                <ul className={styles.resultContents}>
+                    <li>결과</li>
+                    <li>결과</li>
+                    <li>결과</li>
+                    <li>결과</li>
+                </ul>
+            </section>
+            <section className={styles.buttonBox}>
+                <div className={styles.retry}>
+                    <BlackButton buttonTxt="다시하기" />
+                </div>
+                <div className={styles.share}>
+                    <YellowButton buttonTxt="결과 링크 공유하기" />
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Result;
