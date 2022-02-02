b617a12068cbfd067c213ea0f51757d57e66fc0c
codnjs779 <codnjs779@naver.com>
2022-02-02 17:34:07 +0900

    amplify에 환경변수 등록하기 위해 수정

--- src/components/pageScroll/ScrollToTop.jsx
+++ src/components/pageScroll/ScrollToTop.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
